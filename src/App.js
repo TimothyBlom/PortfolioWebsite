80086e832132bc4e04222b781bd861d612bfc977
kracok <kracok@gmail.com>
2021-01-29 13:39:20 +0100

    added layout and pictures

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="introTab">
+        <p>Me (intro)</p>
+      </div>
+      
+      <div className="aboutMeTab">
+        <p>About me (info)</p>
+      </div>
+      
+      <div className="codingTab">
+        <p>Coding projects</p>
+      </div>
+      
+      <div className="modelingTab">
+        <p>3D projects</p>
+      </div>
+      
+      <div className="skillsTab">
+        <p>My programs skills</p>
+      </div>
+     
+      <div className="contactTab">
+        <p>Contact me</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
