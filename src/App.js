dc7e7cad6ef2aff9b0f63703a067165447de5365
kracok <kracok@gmail.com>
2021-02-01 17:39:28 +0100

    text and css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './style/App.css';
 import Tim from "./pictures/Tim.jpg"
 import Logo from "./pictures/Logo.png"
 
@@ -6,9 +6,13 @@ function App() {
   return (
     <div className="App">
 
+      <div className="navigator">
+        nav
+      </div>
+
       <div className="introTab">
-        <img className="profilePicture" src={Tim}/>
-        <img className="logo" src={Logo}/>
+        <img className="profilePicture" alt="" src={Tim}/>
+        <img className="logo" alt="" src={Logo}/>
         <div className="logoText">
           <p>Front-End Developer</p>
           <p>3D Game Artist</p>
@@ -17,10 +21,15 @@ function App() {
       </div>
       
       <div className="aboutMeTab">
-        <p>About me (info)</p>
+        <p>About me</p>
         <p>
-          I am a Front End Developer and a 3D Artist. I live in the Netherlands in a little town called Heerhugowaard. 
-          I spent most of my time playing video games, learning new code, making 3D art and coming up with new cool projects I never end up finishing.
+          My name is Timothy Blom, I am a Front-End Developer and a 3D Artist. I live in the Netherlands in a little town called Heerhugowaard. 
+          I spent most of my time playing video games, reading books, learning new code, making 3D art and coming up with new cool projects I never end up finishing.
+        </p>
+        <p>
+          My goal is to find a job as an Front-End developer. I will achief this by constantly learning new code skills and showing it by 
+          regularly updating my GitHub portfolio, and (the hardest part) by not giving up until I find a job. I have had/have a veriaty of jobs and nothing made 
+          me as happy as creating digital content. Also coders are very cool people and fun to work with.
         </p>
       </div>
       
@@ -33,7 +42,7 @@ function App() {
       </div>
       
       <div className="skillsTab">
-        <p>My programs skills</p>
+        <p>My programs skills (interactive cv)</p>
       </div>
      
       <div className="contactTab">
